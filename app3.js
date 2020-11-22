63f077c63099b59598b039860a1c58e444a0f8a5
stephanie angibert <stephanieangibert@orange.fr>
2020-11-22 06:08:52 +0100

    debug du toggle

--- app3.js
+++ app3.js
@@ -10,7 +10,7 @@ cercle.addEventListener("click",()=>{
     .to(second,{opacity:0.5,ease:Power1.easeOut,duration:1})   
     .to(three,{top:0,left:0,position:"fixed",ease:Power1.easeOut}) 
     .to(three,{opacity:1,ease:Power1.easeOut,duration:0.5})
-    vrai=false;
+    
 
    }else{
     tl 
@@ -20,6 +20,6 @@ cercle.addEventListener("click",()=>{
     .to(three,{opacity:0,ease:Power1.easeOut,duration:0.5})
 
    }
-   vrai!=vrai;
+   vrai = !vrai;
   
 })
